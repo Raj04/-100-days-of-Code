1ee8c96cad5c5a37b701ea184715da2ee105ce2d
Raj_Yadav <raj.yadav@aot.edu.in>
2019-01-28 21:33:45 +0530

    Objects inside an array

---
+++ JS programs/objectsArray.js
@@ -0,0 +1,31 @@
+//objects inside an array
+
+
+var arr=[   
+            {
+                name:"raj",    
+            },
+            {
+                name:"rajdeep",
+            }
+    ];
+    
+    console.log(arr[0].name);
+    
+    
+//2d array
+var arr1=[
+            [
+                {
+                    name:"raj",
+                }    
+            ],
+            [
+                {
+                    name:"rajdeep",
+                }
+            
+            ]
+    ];
+    console.log(arr1[0][0].name);
+    
