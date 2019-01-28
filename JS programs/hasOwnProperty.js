258fbc1478d6c25a3bfc74ad429ac06e654234fa
Raj_Yadav <raj.yadav@aot.edu.in>
2019-01-28 21:34:05 +0530

    Create hasOwnProperty.js

---
+++ JS programs/hasOwnProperty.js
@@ -0,0 +1,11 @@
+ var obj={
+     
+     name:"raj",
+     age:21,
+     project:"Morrisons",
+     profile:"ASE-Trainee",
+ }
+
+ 
+ console.log(obj.hasOwnProperty("name"));
+ console.log(obj.hasOwnProperty("height"));
